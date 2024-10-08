ab9648b6558683b0d19362b8b9451468e5d3686a
ly8529 <1123617175@qq.com>
2024-10-08 21:38:01 +0800

    安装css预处理器sass

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+    <div class="red">hahhah 我是红色</div>
   </div>
 </template>
+<style lang="scss">
+@import url(assets/common/reset.scss);
+</style>
