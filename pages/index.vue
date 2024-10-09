9518452e23578df4179c9306cc965b9a9002bbdf
ly8529 <1123617175@qq.com>
2024-10-09 16:42:55 +0800

    安装多语言

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,40 @@
 	<h1 class="text-3xl font-bold underline" @click="addCount">
 		{{ counterStore.count }}
 	</h1>
-	<nuxt-icon name="art" class="text-32px"></nuxt-icon>
+	<!-- <nuxt-icon name="art" class="text-32px"></nuxt-icon> -->
 	<a-button>ant-btn</a-button>
+	<p>切换中英文：{{ $t('test') }}</p>
+	<a-button @click="changeLang">切换语言</a-button>
 </template>
 <script setup lang="ts">
 import { Button as AButton } from 'ant-design-vue'
 import { useCounterStore } from '@/store/index'
+import { setCurSystemLang, curLang } from '@/plugins/i18n/index'
+import { SystemLang } from '@/global/enum'
+import { APIs } from '@/service/apiList.js'
+import $api from '@/service/webRequest'
 const counterStore = useCounterStore()
 function addCount() {
 	counterStore.increment()
 }
+
+let lang = curLang
+function changeLang() {
+	if (lang === SystemLang.EN) {
+		setCurSystemLang(SystemLang.ZH)
+		lang = SystemLang.ZH
+	} else {
+		setCurSystemLang(SystemLang.EN)
+		lang = SystemLang.EN
+	}
+}
+
+//测试请求
+$api.post(APIs.login)
+	.then(() => {})
+	.catch(err => {
+		console.log(err)
+	})
 </script>
 <style lang="scss">
 @import url(assets/common/reset.scss);
---
+++ plugins/i18n/index.ts
@@ -0,0 +1,42 @@
+import { createI18n } from 'vue-i18n'
+import en from './en'
+import zh from './zh'
+import { SystemLang } from '@/global/enum'
+
+// const localLang = localStorage.SYSTEM_LANG
+
+// let curSystemLang
+// if (localLang) {
+// 	curSystemLang = ['zh', 'zh-cn'].includes(localLang) ? SystemLang.ZH : SystemLang.EN
+// }
+
+// const defaultLang = curSystemLang
+// 	? curSystemLang
+// 	: ['zh', 'zh-cn'].includes(window.navigator.language.toLocaleLowerCase())
+// 		? SystemLang.ZH
+// 		: SystemLang.EN
+
+const defaultLang = SystemLang.ZH
+
+const messages = {
+	en,
+	zh,
+}
+
+const i18n = createI18n({
+	messages,
+	locale: defaultLang,
+	//fallbackLocale: 'en', // 设置回退语言环境
+})
+
+export function setCurSystemLang(curSystemLang: SystemLang) {
+	i18n.global.locale = curSystemLang
+}
+
+export const curLang = i18n.global.locale
+
+export const $i18n = i18n.global.t.bind(i18n.global)
+
+export default defineNuxtPlugin(({ vueApp }) => {
+	vueApp.use(i18n)
+})
