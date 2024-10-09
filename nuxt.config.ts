7f711d29f61646132eb4ba7af03eaf0a5cf9f52d
ly8529 <1123617175@qq.com>
2024-10-09 10:50:03 +0800

    代码风格配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: {
-    enabled: true,
-  },
-  postcss: {
-    plugins: {
-      autoprefixer: {
-        overrideBrowserslist: ["Chrome > 40", "ff > 31", "ie 11"],
-      },
-      tailwindcss: {},
-    },
-  },
-  modules: ["@pinia/nuxt", "nuxt-icons"],
-  // alias: {
-  //   "@": "/<srcDir>",
-  //   assets: "/<srcDir>/assets",
-  //   public: "/<srcDir>/public",
-  // },
-});
+	compatibilityDate: '2024-04-03',
+	devtools: {
+		enabled: true,
+	},
+	postcss: {
+		plugins: {
+			autoprefixer: {
+				overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+			},
+			tailwindcss: {},
+		},
+	},
+	modules: ['@pinia/nuxt', 'nuxt-icons'],
+	// alias: {
+	//   "@": "/<srcDir>",
+	//   assets: "/<srcDir>/assets",
+	//   public: "/<srcDir>/public",
+	// },
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="red filter">hhhh 我是红色字体</div>
-  <h1 class="text-3xl font-bold underline" @click="addCount">
-    {{ counterStore.count }}
-  </h1>
-  <NuxtIcon name="art" class="text-32px"></NuxtIcon>
+	<div class="red filter">hhhh 我是红色字体</div>
+	<h1 class="text-3xl font-bold underline" @click="addCount">
+		{{ counterStore.count }}
+	</h1>
+	<nuxt-icon name="art" class="text-32px"></nuxt-icon>
 </template>
 <script setup lang="ts">
-import { useCounterStore } from "../store/index";
-const counterStore = useCounterStore();
+import { useCounterStore } from '../store/index'
+const counterStore = useCounterStore()
 function addCount() {
-  counterStore.increment();
+	counterStore.increment()
 }
 </script>
 <style lang="scss">
 @import url(assets/common/reset.scss);
 .filter {
-  filter: blur(10px);
-  display: flex;
+	filter: blur(10px);
+	display: flex;
 }
 </style>
